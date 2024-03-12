2e5538cada8de532114389052ebb3a0c653d3eaf
Md. Rajin Mashrur Siam <67337679+rajin-siam@users.noreply.github.com>
2024-03-12 15:53:24 +0600

    Update Sieve of Eratosthenes.cpp

--- Number Theory/Sieve of Eratosthenes.cpp
+++ Number Theory/Sieve of Eratosthenes.cpp
@@ -1,17 +1,32 @@
-void sieve(int n)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int mx = 1e8;
+bitset<mx>isPrime;
+vector<ll>primes;
+void primeGen(ll n)
 {
-  int prime[n+3];
-  memset(prime,0,sizeof(prime));
-  for(ll i=2;i*i<=n;i++)
-  {
-    if(prime[i]==0)
+    for(ll i=3;i<=n;i+=2)isPrime[i]=1;
+    ll sq=sqrt(n);
+    for(ll i=3;i<=sq;i+=2)
     {
-      for(ll j=i*i;j<=n;j+=i)prime[j]=1;
+        if(isPrime[i])
+        {
+            for(ll j=i*i;j<=n;j+=i)
+            {
+                isPrime[j]=0;
+            }
+            
+        }
     }
-  }
-  for(ll i=2;i<=n;i++)
-  {
-    if(prime[i]==0)cout<<i<<" ";
-  }
-  cout<<endl;
+    primes.push_back(2);
+ 
+    for ( int i = 3; i <= n; i += 2 ) {
+        if(isPrime[i] == 1) {
+            primes.push_back(i);
+        }
+    }
+    
 }
